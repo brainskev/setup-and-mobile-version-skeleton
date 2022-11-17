ca9b49e55134c765794e37e1f248cdadec1ce0b5
Lucas David Erkana <lderkana21x@gmail.com>
2022-11-17 13:05:44 +0200

    Add email validator

--- main.js
+++ main.js
@@ -290,6 +290,7 @@ buttonOne.addEventListener('click',()=>{
 
 const [form] = document.getElementsByClassName('contact-form');
 const error = document.querySelector('.error');
+const validEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 form.addEventListener('submit', (e) => {
   const { email } = form.elements;
   const message = [];
